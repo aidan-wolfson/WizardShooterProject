23a2ee7e7a9f4bac3ff73e76953bfcd1bf972913
Y1nno <59004110+Y1nno@users.noreply.github.com>
2023-10-03 12:15:54 -0600

    Gamemap created, teleporter area created

--- player/player.gd
+++ player/player.gd
@@ -8,9 +8,6 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	move(delta)
-	
-	
-	
 
 func get_input_axis():
 	axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
