8cbb37a1061642f8588494e13e1766283582bdf8
aidan-wolfson <aidanawolfson@gmail.com>
2023-10-03 11:23:52 -0600

    Added sprite assets

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@export var MAX_HP = 50
+@export var CURRENT_HP = 50
+
 @export var MAX_SPEED = 300
 @export var ACCELERATION = 15000
 @export var FRICTION = 1200
@@ -15,13 +18,11 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("action_attack") and attackTimer.is_stopped():
 		var projectile_dir = self.global_position.direction_to(get_global_mouse_position())
 		fire_projectile(projectile_dir)
-	
 
 func get_input_axis():
 	axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	axis.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
 	return axis.normalized()
-	
 
 func move(delta):
 	axis = get_input_axis()
@@ -37,7 +38,6 @@ func apply_friction(amount):
 		velocity -= velocity.normalized() * amount
 	else:
 		velocity = Vector2.ZERO
-		
 
 func apply_movement(accel):
 	velocity += accel
@@ -53,3 +53,8 @@ func fire_projectile(projectile_direction: Vector2):
 		projectile.rotation = projectile_rotation #set projectile rot to point at mouse pos
 		
 		attackTimer.start()
+
+func die():
+	queue_free()
+
+
