76b5d7f24c55737d1bb315aa4e9607b102937fce
aidan-wolfson <aidanawolfson@gmail.com>
2024-01-29 12:23:28 -0700

    Added Titan Walk SFX

--- enemy/TitanEnemy.gd
+++ enemy/TitanEnemy.gd
@@ -1,5 +1,7 @@
 extends "res://enemy.gd"
 
+@onready var walkSFX = $walkSound
+
 func _on_enemy_hitbox_area_entered(area):
 	if area.is_in_group("Projectile"):
 		# getting hit by projectile
@@ -11,3 +13,8 @@ func _on_enemy_hitbox_area_entered(area):
 func animationHandler():
 	pass
 	##Overrides enemy base function, used to prevent flipping the sprite
+
+func playWalkSFX():
+	walkSFX.play()
+
+
