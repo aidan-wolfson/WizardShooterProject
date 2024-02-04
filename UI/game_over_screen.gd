f6ead6f904e250f7e15f753a0fd2fce677c5b3b4
aidan-wolfson <aidanawolfson@gmail.com>
2024-02-04 12:45:50 -0700

    Restart Fixes
    The restart function works now lol

--- UI/game_over_screen.gd
+++ UI/game_over_screen.gd
@@ -4,7 +4,8 @@ extends CanvasLayer
 
 
 func _on_restart_pressed():
-	get_tree().change_scene_to_file("res://world/world.tscn")
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://world/NewWorld.tscn")
 
 func _on_quit_pressed():
 	# may not work for all platforms, should check to see which platform and quit accordingly
